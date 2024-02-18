b5c37ecd6a7fc640c6bc0b35c2a94a8c3114bbcb
Abhijith H R <abhijith.hr@ib-systems.org>
2024-02-18 19:46:00 +0100

    added css fix

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import {AppProps} from "next/app"
+import "primereact/resources/primereact.min.css";
+import "primeflex/primeflex.css";
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
+import "primeicons/primeicons.css";
+
+// Import your custom components or layout components
+function MyApp({ Component, pageProps }:AppProps) {
+  // Additional setup or global configurations can be added here
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
