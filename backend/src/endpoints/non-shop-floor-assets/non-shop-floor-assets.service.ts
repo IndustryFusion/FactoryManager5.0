8d04b833e8a9a22b6d0432199412c84672f3505e
sathishKarthikeyan <sathish.k@ib-systems.org>
2024-03-01 14:27:56 +0530

    added asset_category to unallocated assets

--- backend/src/endpoints/non-shop-floor-assets/non-shop-floor-assets.service.ts
+++ backend/src/endpoints/non-shop-floor-assets/non-shop-floor-assets.service.ts
@@ -1,69 +1,32 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { AssetService } from '../asset/asset.service';
-import { FactorySiteService } from '../factory-site/factory-site.service';
-import { ShopFloorService } from '../shop-floor/shop-floor.service';
+import { AllocatedAssetService } from '../allocated-asset/allocated-asset.service';
 
 @Injectable()
 export class NonShopFloorAssetsService {
   constructor(
-    private readonly factorySiteService: FactorySiteService,
     private readonly assetService: AssetService,
-    private readonly shopFloorService: ShopFloorService,
+    private readonly allocatedAssetService: AllocatedAssetService,
   ) {}
 
   async findAll(id: string, token: string) {
     try {
       let assetIds = await this.assetService.getAssetIds(token);
       console.log('assetIds ', assetIds);
-      let factoryData = await this.factorySiteService.findOne(id, token);
-      let shopFloorIds = factoryData['http://www.industry-fusion.org/schema#hasShopFloor'];
-      console.log('shopFloorIds ', shopFloorIds);
-      let hasAssetArr = [];
-      if (Array.isArray(shopFloorIds) && shopFloorIds.length > 0) {
-        for (let i = 0; i < shopFloorIds.length; i++) {
-          let id = shopFloorIds[i].object;
-          if (id.includes('urn')) {
-            let shopFloorData = await this.shopFloorService.findOne(id, token);
-            console.log('shopFloorData ', shopFloorData);
-            let hasAsset = shopFloorData['http://www.industry-fusion.org/schema#hasAsset'];
-            if (Array.isArray(hasAsset) && hasAsset.length > 0) {
-              hasAsset.forEach((data) => {
-                hasAssetArr.push(data.object);
-              })
-            } else if (hasAsset.object.includes('urn')) {
-              hasAssetArr = [...hasAssetArr, hasAsset.object];
-            }
-          }
-        }
-      } else if (shopFloorIds.object.includes('urn')) {
-        let shopFloorData = await this.shopFloorService.findOne(
-          shopFloorIds.object,
-          token,
-        );
-        console.log('shopFloorData ', shopFloorData);
-        let hasAsset = shopFloorData['http://www.industry-fusion.org/schema#hasAsset'];
-        if (Array.isArray(hasAsset) && hasAsset.length > 0) {
-          hasAsset.forEach((data) => {
-            hasAssetArr.push(data.object);
-          })
-        } else if (hasAsset.object.includes('urn')) {
-          hasAssetArr = [...hasAssetArr, hasAsset.object];
-        }
-      }
-      console.log(hasAssetArr, ' hasArray');
-      if (hasAssetArr.length > 0) {
+      let allocatedAssets = await this.allocatedAssetService.findAll(token);
+      if (allocatedAssets.length > 0) {
         const filteredArray = [];
         for (let i = 0; i < assetIds.length; i++) {
           let id = assetIds[i];
-          if (!hasAssetArr.includes(id)) {
+          if (!allocatedAssets.includes(id)) {
             const assetData = await this.assetService.getAssetDataById(
               id,
               token,
             );
             const filteredObject = {
               id,
-              product_name:
-                assetData['http://www.industry-fusion.org/schema#product_name'],
+              product_name: assetData['http://www.industry-fusion.org/schema#product_name'],
+              asset_category: assetData['http://www.industry-fusion.org/schema#asset_category']
             };
             for (const key in assetData) {
               if (key.includes('has')) {
@@ -78,23 +41,21 @@ export class NonShopFloorAssetsService {
         const filteredArray = [];
         for (let i = 0; i < assetIds.length; i++) {
           let id = assetIds[i];
-          if (!hasAssetArr.includes(id)) {
-            const assetData = await this.assetService.getAssetDataById(
-              id,
-              token,
-            );
-            const filteredObject = {
-              id,
-              product_name:
-                assetData['http://www.industry-fusion.org/schema#product_name'],
-            };
-            for (const key in assetData) {
-              if (key.includes('has')) {
-                filteredObject[key] = assetData[key];
-              }
+          const assetData = await this.assetService.getAssetDataById(
+            id,
+            token,
+          );
+          const filteredObject = {
+            id,
+            product_name: assetData['http://www.industry-fusion.org/schema#product_name'],
+            asset_category: assetData['http://www.industry-fusion.org/schema#asset_category']
+          };
+          for (const key in assetData) {
+            if (key.includes('has')) {
+              filteredObject[key] = assetData[key];
             }
-            filteredArray.push(filteredObject);
           }
+          filteredArray.push(filteredObject);
         }
         return filteredArray;
       }
