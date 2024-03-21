a894934f74f347d72b515ccbe2b2360b32fe6f71
sathishKarthikeyan <sathish.k@ib-systems.org>
2024-03-21 16:56:51 +0530

    created machine-state graph

--- frontend/src/utility/chart-utility.ts
+++ frontend/src/utility/chart-utility.ts
@@ -1,60 +1,3 @@
-export const machineData = [
-  {
-    "observedAt": "2024-03-05T15:08:53.823+00:00",
-    "modifiedAt": "2024-03-05T14:08:53.823+00:00",
-    "entityId": "urn:ngsi-ld:asset:2:101",
-    "attributeId": "http://www.industry-fusion.org/fields#machine-state",
-    "attributeType": "https://uri.etsi.org/ngsi-ld/Property",
-    "datasetId": "urn:ngsi-ld:asset:2:101\\http://www.industry-fusion.org/fields#machine-state",
-    "nodeType": "@value",
-    "value": "0",
-    "valueType": null,
-    "index": 0,
-    "prev_value": "2"
-  },
-  {
-    "observedAt": "2024-03-05T20:20:38.551+00:00",
-    "modifiedAt": "2024-03-05T15:20:38.551+00:00",
-    "entityId": "urn:ngsi-ld:asset:2:101",
-    "attributeId": "http://www.industry-fusion.org/fields#machine-state",
-    "attributeType": "https://uri.etsi.org/ngsi-ld/Property",
-    "datasetId": "urn:ngsi-ld:asset:2:101\\http://www.industry-fusion.org/fields#machine-state",
-    "nodeType": "@value",
-    "value": "2",
-    "valueType": null,
-    "index": 0,
-    "prev_value": "0"
-  },
-  {
-    "observedAt": "2024-03-07T20:00:08.551+00:00",
-    "modifiedAt": "2024-03-05T15:20:38.551+00:00",
-    "entityId": "urn:ngsi-ld:asset:2:101",
-    "attributeId": "http://www.industry-fusion.org/fields#machine-state",
-    "attributeType": "https://uri.etsi.org/ngsi-ld/Property",
-    "datasetId": "urn:ngsi-ld:asset:2:101\\http://www.industry-fusion.org/fields#machine-state",
-    "nodeType": "@value",
-    "value": "2",
-    "valueType": null,
-    "index": 0,
-    "prev_value": "0"
-  },
-  {
-    "observedAt": "2024-03-07T22:08:53.823+00:00",
-    "modifiedAt": "2024-03-05T14:08:53.823+00:00",
-    "entityId": "urn:ngsi-ld:asset:2:101",
-    "attributeId": "http://www.industry-fusion.org/fields#machine-state",
-    "attributeType": "https://uri.etsi.org/ngsi-ld/Property",
-    "datasetId": "urn:ngsi-ld:asset:2:101\\http://www.industry-fusion.org/fields#machine-state",
-    "nodeType": "@value",
-    "value": "0",
-    "valueType": null,
-    "index": 0,
-    "prev_value": "2"
-  },
-
-];
-
-
 export const formatDate = (date: Date) => {
   const day = date.getDate();
   const month = date.getMonth() + 1; // Months are zero-based
@@ -91,7 +34,7 @@ export const mapBackendDataToAssetState = (backendData: any) => {
 };
 
 export const convertToSeconds = (timeData: any) => {
-  const secondsData = timeData.map((time) => {
+  const secondsData = timeData.map((time: string) => {
     const [hours, minutes, seconds] = time.split(':').map(Number);
     return hours * 3600 + minutes * 60 + seconds;
   });
@@ -122,44 +65,6 @@ export function convertToSecondsTime(time) {
   }
 
   const [hours, minutes, seconds] = time.split(':').map(Number);
-  return hours * 3600 + minutes * 60 + seconds;
+  return hours * 3600 + minutes * 60 + seconds; 
 }
-
-export const groupedByDate = {};
-machineData.forEach(item => {
-  const date = item.observedAt.split('T')[0];
-  const time = item.observedAt.split('T')[1].split('.')[0];
-  if (!groupedByDate[date]) {
-    groupedByDate[date] = { online: [], offline: [] };
-  }
-  if (item.prev_value === "0") {
-    groupedByDate[date].offline.push(time);
-  } else if (item.prev_value === "2") {
-    groupedByDate[date].online.push(time);
-  }
-});
-
-Object.keys(groupedByDate).forEach(date => {
-  const { online, offline } = groupedByDate[date];
-  const maxLength = Math.max(online.length, offline.length);
-  // Ensure both arrays have the same length
-  while (online.length < maxLength) online.push('00:00:00');
-  while (offline.length < maxLength) offline.push('00:00:00');
-  // Iterate over indices and adjust times
-  for (let i = 0; i < maxLength; i++) {
-    const onlineTime = convertToSecondsTime(online[i]);
-    const offlineTime = convertToSecondsTime(offline[i]);
-    const difference = Math.abs(onlineTime - offlineTime);
-    console.log(convertSecondstoTime(onlineTime), "onlineTime data");
-    console.log(convertSecondstoTime(offlineTime), "offlineTime data");
-    console.log(difference, "what's the difference");
-    console.log(convertSecondstoTime(difference), "difference timeformat");
-
-    if (onlineTime > offlineTime) {
-      online[i] = convertSecondstoTime(difference);
-    } else {
-      offline[i] = convertSecondstoTime(difference);
-    }
-  }
-});
 
