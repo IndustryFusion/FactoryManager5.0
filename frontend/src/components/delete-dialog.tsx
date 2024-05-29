fc568b3ef04684352030f4a52eeb82122e179a30
bhavanas-ib <bhavana.s@ib-systems.org>
2024-05-29 11:17:22 +0530

    added delete dialog

--- frontend/src/components/delete-dialog.tsx
+++ frontend/src/components/delete-dialog.tsx
@@ -14,35 +14,37 @@ interface DeleteDialogProps {
 const DeleteDialog: React.FC<DeleteDialogProps> = ({ deleteDialog, setDeleteDialog, handleDelete, deleteItemName }) => {
     const { t } = useTranslation(['overview']);
 
+    const header = () => <h3>{t('overview:confirmation')}</h3>
     return (
         <>
             <Dialog
                 visible={deleteDialog}
                 onHide={() => setDeleteDialog(false)}
-                header="Confirm Delete"
+                header={header}
             >
                 <div className="mb-6">
-               {/* // {t('dashboard:productName')} */}
                     <span>{t('overview:deleteWarning')}</span>
                     <span style={{ textTransform: "capitalize", fontWeight: "bold" }}> {deleteItemName}</span>
                 </div>
 
 
+                <div className="flex justify-content-end">
+                    <Button
+                        label={t('yes')}
+                        icon="pi pi-check"
+                        className="mr-2"
+                        onClick={handleDelete}
+                    >
+                    </Button>
+                    <Button
+                        label={t('no')}
+                        icon="pi pi-times"
+                        severity="danger" outlined
+                        className="ml-3"
+                        onClick={() => setDeleteDialog(false)}
+                    ></Button>
+                </div>
 
-                <Button
-                    label={t('yes')}
-                    icon="pi pi-check"
-                    className="mr-2"
-                    onClick={handleDelete}
-                >
-                </Button>
-                <Button
-                    label={t('no')}
-                    icon="pi pi-times"
-                    severity="danger" outlined
-                    className="mr-2"
-                    onClick={() => setDeleteDialog(false)}
-                ></Button>
             </Dialog>
         </>
     )
