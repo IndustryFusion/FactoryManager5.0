0a49e5742e9ef5ce275d42a448f289aab03164f6
Vasanth2329 <vasanth.a@ib-systems.org>
2024-10-21 20:50:10 +0530

    Contract related pages

--- frontend/src/pages/contract-manager.tsx
+++ frontend/src/pages/contract-manager.tsx
@@ -1,44 +1,96 @@
-import React, { useEffect, useState, useRef } from 'react';
+import Navbar from "@/components/navBar/navbar";
+import React, { useEffect, useState, useRef } from "react";
 import "../styles/contract-manager.css";
-import ContractHeader from '@/components/contractManager/contract-header';
-import { InputText } from 'primereact/inputtext';
-import { Tree } from 'primereact/tree';
-import { NodeService } from '@/service/NodeService';
-import ContractCards from '@/components/contractManager/contract-cards';
-import { Checkbox } from 'primereact/checkbox';
-import { getAllContract } from '@/utility/contract';
-import { getAccessGroup } from "../utility/indexed-db";
-import { Toast } from 'primereact/toast';
+import ContractHeader from "@/components/contractManager/contract-header";
+import { InputText } from "primereact/inputtext";
+import { Tree } from "primereact/tree";
+import { NodeService } from "@/service/NodeService";
+import { Checkbox } from "primereact/checkbox";
+import { getAccessGroup } from "@/utility/indexed-db";
+import { getContracts } from "@/utility/contract";
+import ContractCard from "@/components/contractManager/contract-file";
+import { IoArrowBack } from "react-icons/io5";
+import ContractFolders from "@/components/contractManager/contract-folders";
+import { Toast, ToastMessage } from "primereact/toast";
+import axios from "axios";
+import { fetchContractsRedux } from "@/redux/contract/contractSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const ContractManager = () => {
-    const [nodes, setNodes] = useState([]); 
-    const [selectedKey, setSelectedKey] = useState('');
-    const toast = useRef<Toast>(null);
+  const [nodes, setNodes] = useState([]);
+  const [companyIfricId, setCompanyIfricId] = useState("");
+  const [selectedKey, setSelectedKey] = useState("");
+  // const [contractsData, setContractsData] = useState([]);
+  const [predictiveFilteredContractsData, setpredictiveFilteredContractsData] =
+    useState([]);
+  const [filterContracts, setFilterContracts] = useState(false);
+  const [insuranceFilterContracts, setInsuranceFilterContracts] =
+    useState(false);
+  const [contractsOriginal, setContractsOriginal] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const toast = useRef<Toast>(null);
+  const dispatch = useDispatch();
 
-    const fetchContract = async () => {
-      try {
-          const userData = await getAccessGroup();
-          if (userData && userData.jwt_token) {
-            const response = await getAllContract(userData.company_ifric_id);
-            console.log("response for getAllContract ",response);
-          } else {
-            toast.current?.show({ severity: 'error', summary: 'Error', detail: 'User data or JWT not found' });
-          }
-      } catch (error: any) {
-        console.error('Error fetching data:', error);
-        if (error?.response?.message) {
-          toast.current?.show({ severity: 'error', summary: 'Error', detail: error.response.message });
-        } else {
-          // For manually thrown errors
-          toast.current?.show({ severity: 'error', summary: 'Error', detail: error.message || 'Unknown error occurred' });
-        }
-      }
+  // Access the contracts data from Redux
+  const contractsData = useSelector((state: any) => state.contracts.contracts);
+
+  const showToast = (
+    severity: ToastMessage["severity"],
+    summary: string,
+    message: string
+  ) => {
+    toast.current?.show({
+      severity: severity,
+      summary: summary,
+      detail: message,
+      life: 8000,
+    });
   };
 
-    useEffect(() => {
-        NodeService.getTreeNodes().then((data) => setNodes(data));
-        fetchContract();
-    }, []);
+  useEffect(() => {
+    NodeService.getTreeNodes().then((data) => setNodes(data));
+  }, []);
+
+  const getCompanyId = async () => {
+    try {
+    const details = await getAccessGroup();
+    setCompanyIfricId(details.company_ifric_id);
+    dispatch(fetchContractsRedux(companyIfricId));
+    } catch(error: any) {
+      if (axios.isAxiosError(error)) {
+        console.error("Error response:", error.response?.data.message);
+        showToast("error", "Error", "Fetching assets");
+      } else {
+        console.error("Error:", error);
+        showToast("error", "Error", error);
+      }
+    }
+  };
+  useEffect(() => {
+    getCompanyId();
+  });
+
+
+
+  const handleFilterContracts = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      const filteredData = contractsData.filter(
+        (contract) =>
+          contract?.contract_type?.trim() ===
+          "https://industry-fusion.org/contracts/v0.1/predictiveMaintenanceLaserCutter"
+      );
+      setpredictiveFilteredContractsData(filteredData);
+      setLoading(false);
+    }, 2000); // Adjust the delay time in milliseconds (e.g., 1000 = 1 second)
+  };
+
+  useEffect(() => {
+    if (filterContracts) {
+      handleFilterContracts();
+    }
+  }, [filterContracts]);
 
   return (
     <>
@@ -46,52 +98,118 @@ const ContractManager = () => {
         <Toast ref={toast} />
         <div className="main_content_wrapper">
           <div className="navbar_wrapper">
-            <div className='flex gap-4 contract-container'>
-                <div className='contract-left-container'>
-                <div className="contract-search-container"> 
-              <InputText
-                className="contract-search-input"
-                // value={globalFilterValue}
-                // onChange={onFilter}
-                placeholder="Search contracts"
-              />
-              <img 
-              className="search-expand"
-              src="/search_icon.svg" alt="search-icon" />
-            </div>
-            <div className='mt-6'>
-                <h3 className='m-0 ml-1 folder-heading'>Folders</h3>
-              <div className="card flex mt-1 contracts-tree">
-            <Tree
-            value={nodes} selectionMode="single" selectionKeys={selectedKey} onSelectionChange={(e) => setSelectedKey(e.value)}  />
-            </div>
-            </div>
-            <div className='mt-6'>
-            <h3 className='m-0 ml-1 folder-heading'>Simple Lawfirm Members</h3>
-            <div className='flex gap-3'>
-                <p className='card-label-grey' style={{textDecoration:"underline"}}>Select All</p>
-                <p className='card-label-grey' style={{textDecoration:"underline"}}>Unselect All</p>
-            </div>
-            <div className='flex gap-2 align-items-center'>
-                <Checkbox />
-                <p className='m-0'>Ewelina</p>
-            </div>
-            <div className='flex gap-2 align-items-center mt-2'>
-                <Checkbox />
-                <p className='m-0'>Patt Member</p>
-            </div>
-            </div>
+            <Navbar navHeader="Contracts Manager" />
+            <div className="flex gap-4 contract-container">
+              <div className="contract-left-container">
+                <div className="contract-search-container">
+                  <InputText
+                    className="contract-search-input"
+                    // value={globalFilterValue}
+                    // onChange={onFilter}
+                    placeholder="Search contracts"
+                  />
+                  <img
+                    className="search-expand"
+                    src="/search_icon.svg"
+                    alt="search-icon"
+                  />
                 </div>
-                <div className='contract-right-container'>
-                  <ContractHeader />
-                  <ContractCards />
+                <div className="mt-6">
+                  <h3 className="m-0 ml-1 folder-heading">Folders</h3>
+                  <div className="card flex mt-1 contracts-tree">
+                    <Tree
+                      value={nodes}
+                      selectionMode="single"
+                      selectionKeys={selectedKey}
+                      onSelectionChange={(e) => setSelectedKey(e.value)}
+                    />
+                  </div>
                 </div>
+                <div className="mt-6">
+                  <div className="flex gap-3">
+                    <p
+                      className="card-label-grey"
+                      style={{ textDecoration: "underline" }}
+                    >
+                      Select All
+                    </p>
+                    <p
+                      className="card-label-grey"
+                      style={{ textDecoration: "underline" }}
+                    >
+                      Unselect All
+                    </p>
+                  </div>
+                  <div className="flex gap-2 align-items-center">
+                    <Checkbox />
+                    <p className="m-0">Ewelina</p>
+                  </div>
+                  <div className="flex gap-2 align-items-center mt-2">
+                    <Checkbox />
+                    <p className="m-0">Patt Member</p>
+                  </div>
+                </div>
+              </div>
+              <div className="contract-right-container">
+                <ContractHeader />
+                <div className="contract-cards-container">
+                  <ContractFolders
+                    setFilterContracts={setFilterContracts}
+                    setInsuranceFilterContracts={setInsuranceFilterContracts}
+                    setContractsOriginal={setContractsOriginal}
+                    contractsOriginal={contractsOriginal}
+                  />
+                  {loading ? (
+                    <div></div>
+                  ) : (
+                    <>
+                      {!contractsOriginal && (
+                        <div className="ml-1">
+                          <button
+                            className="back-btn flex justify-content-center align-items-center border-none black_button_hover "
+                            onClick={() => {
+                              setInsuranceFilterContracts(false);
+                              setFilterContracts(false);
+                              setContractsOriginal(true);
+                            }}
+                          >
+                            <IoArrowBack className="mr-1" />
+                            <span className="mt-1">Back</span>
+                          </button>
+                        </div>
+                      )}
+                      {filterContracts &&
+                        predictiveFilteredContractsData.length > 0 &&
+                        predictiveFilteredContractsData.map((contract) => (
+                          <div  key={contract._id}>
+                             <ContractCard
+                            contract={contract}
+                          />
+                          </div> 
+                        ))}
+                      {insuranceFilterContracts && (
+                        <div>
+                          <h3 className="not-found-text">
+                            Insurance contract files not found
+                          </h3>
+                        </div>
+                      )}
+                      {contractsOriginal &&
+                        contractsData.map((contract) => (
+                          <div key={contract._id}>
+                            <ContractCard contract={contract} />
+                          </div>
+                        ))}
+                    </>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ContractManager
+export default ContractManager;
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -21,7 +21,8 @@ import entityIdReducer from "./entityId/entityIdSlice";
 import authReducer from "./auth/authSlice"
 import relationsReducer from "./relations/relationsSlice";
 import powerConsumptionReducer from "./powerConsumption/powerConsumptionSlice";
-import assetManagementReducer from './assetManagement/assetManagementSlice'
+import assetManagementReducer from './assetManagement/assetManagementSlice';
+import contractsSliceReducer from "./contract/contractSlice";
 
 export const store = configureStore({
     reducer: {
@@ -32,6 +33,7 @@ export const store = configureStore({
         relations: relationsReducer,
         powerConsumption: powerConsumptionReducer,
         assetManagement: assetManagementReducer,
+        contracts: contractsSliceReducer
     },
 });
 
