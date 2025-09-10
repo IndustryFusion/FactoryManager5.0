06cf4038d0cb6e49bb8db60bdf3c5efffaeca056
Abhijith H R <abhijith.hr@ib-systems.org>
2025-09-10 17:25:53 +0200

    XANA Icon updates

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -286,7 +286,7 @@ const DashboardPage: React.FC = () => {
                                     onClick={handleXanaOpen}
                                     style={{ cursor: "pointer" }}
                                 >
-                                    <span><img style={{ width: "32px", height: "32px" }} src="/ai-audio.svg" /></span>Ask Xana AI
+                                    <span><img style={{ width: "28px", height: "28px" }} src="/ai-audio.svg" /></span>Ask Xana AI
                                 </button>
                             </div>
                         </div>
